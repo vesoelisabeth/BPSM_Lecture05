c134e9f74906b33c76ab24e1308a84be210388d9
vesoelisabeth <vesoelisabeth@gmail.com>
2024-10-22 12:54:58 +0100

    Python exercises

---
+++ Lecture11/lecture11ex.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+dna_sequence = ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT
+a_count = dna_sequence.count("A")
+print ("There are", str(a_count), "A nucleotides in the sequence" 
+t_count = dna_sequence.count("T")
+print ("There are" + str(t_count) + "T nucleotides in the sequences"
+
+combined_seq = a_count + t_count
+leng_seq = len(dna_sequence)
+combined_seq / leng_seq
+
+
