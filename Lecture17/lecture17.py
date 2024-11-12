386d098f4a9e6c1e5006dfda3b6690bb1248f173
vesoelisabeth <vesoelisabeth@gmail.com>
2024-11-12 13:00:03 +0000

    Lecture 7 for Python

---
+++ Lecture17/lecture17.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#Lecture notes for the 7th Python lecture
+import os, sys, subprocess
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv('eukaryotes.txt',sep="\t")
+df
+
