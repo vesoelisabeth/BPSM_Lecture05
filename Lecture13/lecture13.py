08c3bbeff244ffccde2c91d9b99ec7986fd0a281
vesoelisabeth <vesoelisabeth@gmail.com>
2024-12-09 23:53:57 +0000

    Python Lecture 3

--- Lecture13/lecture13.py
+++ Lecture13/lecture13.py
@@ -1,2 +1,19 @@
 #! usr/bin/env python3
 
+#for loop from one of the exercises
+
+somecolours4 =  ['blue', 'green', 'red']
+for x in somecolours4 :
+	myfile= open(x + '.txt', 'w')
+	myfile.write(str(len(x)) + '\n')
+	myfile.close()
+	print(f'{x[0:3].upper()}')
+
+#Output
+#2
+#'BLU'
+#2
+#'RED'
+#2
+#'GRE'
+
